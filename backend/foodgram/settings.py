6c0a488771314194704ef953247ddf3622bd31e1
“Елена <borovina@bk.ru>
2022-07-21 19:05:09 +0300

    «i»

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -68,8 +68,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
