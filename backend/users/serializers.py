ee64f754d2942690040f28412892351fbeb114c7
“Елена <borovina@bk.ru>
2022-07-23 14:05:08 +0300

    «in»

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,7 +2,6 @@ from api.models import Recipe
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from users.models import Follow, User
 
 
