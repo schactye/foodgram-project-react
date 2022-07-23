c3fca2e964eb81010320f616896264ca6b430a41
“Елена <borovina@bk.ru>
2022-07-23 14:20:20 +0300

    «i»

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 from api.views import IngredientsViewSet, RecipeViewSet, TagsViewSet
+from rest_framework.routers import DefaultRouter
+
 
 router = DefaultRouter()
 router.register('tags', TagsViewSet, basename='tags')
